93d1cddcb46ca671a594d18561e84af6b3d35ba0
Skorodinskaya <skorodinskaya01@gmail.com>
2021-11-14 21:53:52 +0200

    practice 1 - fixed

--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {Post} from "../../models/post";
 
 @Component({
@@ -6,14 +6,11 @@ import {Post} from "../../models/post";
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.css']
 })
-export class PostComponent implements OnInit {
+export class PostComponent {
 
   @Input()
   post: Post;
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
