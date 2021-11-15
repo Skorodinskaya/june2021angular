a3ce51ae92a00a0fbe59fddd069a7a9fe6f0c98a
Skorodinskaya <skorodinskaya01@gmail.com>
2021-11-16 00:11:04 +0200

    lesson 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UsersComponent } from './components/users/users.component';
+import { UserComponent } from './components/user/user.component';
+import { HttpClientModule } from "@angular/common/http";
+import { RouterModule } from "@angular/router";
+import { PostsComponent } from './components/posts/posts.component';
+import { PostComponent } from './components/post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    UserComponent,
+    PostsComponent,
+    PostComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path: 'users', component: UsersComponent},
+      {path: 'posts', component: PostsComponent},
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
