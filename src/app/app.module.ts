9919f438fb629a95309d55659010384b02371171
Skorodinskaya <skorodinskaya01@gmail.com>
2021-11-20 13:23:30 +0200

    lesson 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import {HomeComponent} from './components/home/home.component';
 import {TestGuard} from "./guards/test.guard";
 import {FormsComponent} from './components/forms/forms.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { TestComponent } from './components/test/test.component';
 
 const routes:Route[] = [
   {
@@ -57,7 +58,8 @@ const routes:Route[] = [
     UserDetailsComponent,
     PostDetailsComponent,
     HomeComponent,
-    FormsComponent
+    FormsComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
